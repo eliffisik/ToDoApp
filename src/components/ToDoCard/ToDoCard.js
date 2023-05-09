0c7af55648473d2cdddf5b628f1d88be45bf7539
eliffisik <eliffisik@icloud.com>
2023-05-09 13:09:17 +0300

    addedfiles

---
+++ src/components/ToDoCard/ToDoCard.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+import styles from './ToDoCard.style';
+import Main from '../Main';
+
+
+
+
+  export default ToDoCard;
